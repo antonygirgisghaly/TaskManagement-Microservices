fb5d69aa2aa4823d4253fcbcb3b50a6d9e3ba31b
Antony Girgis <antony26429122@gmail.com>
2026-08-16 20:21:22 +0300

    feat: Add UserRepository

---
+++ Identity.Domain/Comman/IUserRepository.cs
@@ -0,0 +1,15 @@
+﻿using Identity.Domain.Entites;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Identity.Domain.Comman
+{
+    public interface IUserRepository
+    {
+        Task<User?> GetUserByEmailAsync(string email, CancellationToken cancellationToken = default);
+        Task AddUserAsync(User user, CancellationToken cancellationToken = default);
+        Task UpdateAsync(User user, CancellationToken cancellationToken = default);
+        Task<bool> ExistsUserByEmailAsync(string email, CancellationToken cancellationToken = default);
+    }
+}
---
+++ Identity.Infrastructure/Repository/UserRepository.cs
@@ -0,0 +1,30 @@
+﻿
+using Identity.Domain.Comman;
+using Identity.Domain.Entites;
+using Microsoft.EntityFrameworkCore;
+namespace Identity.Infrastructure.Repository
+{
+    public class UserRepository :IUserRepository
+    {
+        private readonly IdentityDbContext _context;
+        public UserRepository(IdentityDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddUserAsync(User user, CancellationToken cancellationToken = default)
+          => await _context.Users.AddAsync(user, cancellationToken);
+
+        public async Task<bool> ExistsUserByEmailAsync(string email, CancellationToken cancellationToken = default)
+         => await _context.Users.AnyAsync(u => u.Email == email, cancellationToken);
+
+        public async Task<User?> GetUserByEmailAsync(string email, CancellationToken cancellationToken = default)
+         => await _context.Users.FirstOrDefaultAsync(u => u.Email == email, cancellationToken);
+
+        public async Task UpdateAsync(User user, CancellationToken cancellationToken = default)
+        {
+            _context.Users.Update(user);
+            await _context.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
