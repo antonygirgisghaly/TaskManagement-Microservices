5166c87795116d9bf14c60fb5d83e97ca98f000e
Antony Girgis <antony26429122@gmail.com>
2026-08-16 15:20:54 +0300

    feat: Add User Entity

---
+++ Identity.Domain/Entites/User.cs
@@ -0,0 +1,18 @@
+﻿using Identity.Domain.enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Identity.Domain.Entites
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string FullName { get; set; } = default!;
+        public int MyProperty { get; set; }
+        public string Email { get; set; } = default!;
+        public string PasswordHash { get; set; } = default!;
+        public UserRole Role { get; set; } = UserRole.User;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
---
+++ Identity.Domain/enums/UserRole.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Identity.Domain.enums
+{
+    public enum UserRole
+    {
+        User = 0,
+        Admin = 1
+    }
+}
