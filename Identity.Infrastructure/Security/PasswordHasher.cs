4d6473357c8e93b3eaf5195344b411a1772549bf
Antony Girgis <antony26429122@gmail.com>
2026-08-18 12:14:49 +0300

    feat : Add passwordHasher

---
+++ Identity.Infrastructure/Security/PasswordHasher.cs
@@ -0,0 +1,20 @@
+﻿using Identity.Infrastructure.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Identity.Infrastructure.Security
+{
+    internal class PasswordHasher : IPasswordHasher
+    {
+        public string Hash(string password)
+        {
+            return BCrypt.Net.BCrypt.HashPassword(password);
+        }
+
+        public bool Verify(string password, string passwordHash)
+        {
+            return BCrypt.Net.BCrypt.Verify(password, passwordHash);
+        }
+    }
+}
