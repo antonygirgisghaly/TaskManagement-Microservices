fdefcad23aecefe88ffd03bb0af5175630f4c381
Antony Girgis <antony26429122@gmail.com>
2026-08-20 12:45:08 +0300

    feat: Add AuthController

---
+++ Identity.Api/Controllers/AuthController.cs
@@ -0,0 +1,38 @@
+﻿using Identity.Application.Comman;
+using Identity.Application.DTOs;
+using Identity.Application.Services;
+using Identity.Infrastructure.Settings;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+namespace Identity.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+        [HttpPost("Register")]
+        public async Task<ActionResult> Register([FromBody] RegisterRequestDto request)
+        {
+            var result = await _authService.RegisterAsync(request);
+            if (!result.IsSuccess)
+                return NotFound(new { error = result.Error });
+            return Ok(result.Data);
+        }
+        [HttpPost("Login")]
+        public async Task<ActionResult> Login([FromBody] LoginRequestDto request)
+        {
+            var result = await _authService.LoginAsync(request);
+            if (!result.IsSuccess)
+                return NotFound(new { error = result.Error });
+            return Ok(result.Data);
+        }
+    }
+}
--- Identity.Api/Program.cs
+++ Identity.Api/Program.cs
@@ -1,11 +1,16 @@
 using Identity.Application.Interfaces;
+using Identity.Application.Services;
 using Identity.Domain.Comman;
 using Identity.Infrastructure;
 using Identity.Infrastructure.Interfaces;
 using Identity.Infrastructure.Repository;
+using Identity.Infrastructure.Security;
 using Identity.Infrastructure.Settings;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 namespace Identity.Api
 {
     public class Program
@@ -25,8 +30,36 @@ namespace Identity.Api
             builder.Services.Configure<JwtSettings>(builder.Configuration.GetSection("JwtSettings"));
             builder.Services.AddScoped<IPasswordHasher, PasswordHasher>();
             builder.Services.AddScoped<ITokenService, TokenService>();
-            var app = builder.Build();
+            builder.Services.AddScoped<IAuthService, AuthService>();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+            var jwtSettings = builder.Configuration.GetSection("JwtSettings").Get<JwtSettings>();
 
+            builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = jwtSettings!.Issuer,
+                    ValidAudience = jwtSettings.Audience,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings.SecretKey))
+                };
+            });
+
+                var app = builder.Build();
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
@@ -35,6 +68,7 @@ namespace Identity.Api
 
             app.UseHttpsRedirection();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
 
