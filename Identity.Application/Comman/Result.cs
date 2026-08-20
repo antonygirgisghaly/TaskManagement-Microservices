d15f27f1b73c845eeef9e14ac0c45b6b73cab272
Antony Girgis <antony26429122@gmail.com>
2026-08-20 12:44:41 +0300

    feat: Immplment Result Pattern

---
+++ Identity.Application/Comman/Result.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Identity.Application.Comman
+{
+    public class Result<T> 
+    {
+        public bool IsSuccess { get; set; }
+        public T? Data { get; set; } 
+        public string? Error { get; set; }
+
+        public Result(bool isSuccess, T? data = default!, string? error = default!)
+        {
+            IsSuccess = isSuccess;
+            Data = data;
+            Error = error;
+        }
+
+        public static Result<T> Success(T data) => new Result<T>(true, data);
+        public static Result<T> Failure(string error) => new Result<T>(false,default!, error);
+    }
+}
