48943cf520877f627049aa6aeef828e2cb453a50
Antony Girgis <antony26429122@gmail.com>
2026-08-25 17:24:54 +0300

    feat : Add TaskItem

---
+++ Tasks.Domain/Entities/TaskItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Tasks.Domain.Enums;
+
+namespace Tasks.Domain.Entities
+{
+    public class TaskItem
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; } = default!;
+        public string Description { get; set; } = default!;
+        public TaskItemStatus Status { get; set; } = TaskItemStatus.ToDo;
+        public Guid AssignedToUserId { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime? DueDate { get; set; }
+    }
+}
