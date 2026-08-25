ca26b0a0c409e87291327218237f56bbcc5767d6
Antony Girgis <antony26429122@gmail.com>
2026-08-25 17:24:32 +0300

    feat : immplment Reposatory pattern

---
+++ Tasks.Application/Interfaces/ITaskReposatory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Tasks.Domain.Entities;
+
+namespace Tasks.Application.Interfaces
+{
+    public interface ITaskReposatory
+    {
+        Task<TaskItem?> GetByIdAsync(Guid id, CancellationToken ct = default);
+        Task<List<TaskItem>> GetAllByUserIdAsync(Guid userId, CancellationToken ct = default);
+        Task AddAsync(TaskItem task, CancellationToken ct = default);
+        Task UpdateAsync(TaskItem task, CancellationToken ct = default);
+        Task DeleteAsync(TaskItem task, CancellationToken ct = default);
+    }
+}
---
+++ Tasks.Infrastructure/Reposatories/TaskReposatoriey.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Tasks.Application.Interfaces;
+using Tasks.Domain.Entities;
+using Tasks.Infrastructure.DbContexts;
+namespace Tasks.Infrastructure.Reposatories
+{
+    public class TaskReposatoriey : ITaskReposatory
+    {
+        private readonly TasksDbContext _dbContext;
+        public TaskReposatoriey(TasksDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public async Task AddAsync(TaskItem task, CancellationToken ct = default)
+        {
+             await _dbContext.Tasks.AddAsync(task,ct);
+             await _dbContext.SaveChangesAsync(ct);
+        }
+
+        public async Task DeleteAsync(TaskItem task, CancellationToken ct = default)
+        {
+             _dbContext.Tasks.Remove(task); 
+            await _dbContext.SaveChangesAsync(ct);
+        }
+
+        public async Task<List<TaskItem>> GetAllByUserIdAsync(Guid userId, CancellationToken ct = default)
+        {
+            return await _dbContext.Tasks.Where(t => t.AssignedToUserId == userId).ToListAsync(ct);
+        }
+
+        public async Task<TaskItem?> GetByIdAsync(Guid id, CancellationToken ct = default)
+        {
+            return await _dbContext.Tasks.FindAsync(id, ct);
+        }
+
+        public async Task UpdateAsync(TaskItem task, CancellationToken ct = default )
+        {
+            _dbContext.Tasks.Update(task);
+            await _dbContext.SaveChangesAsync(ct);
+        }
+    }
+}
