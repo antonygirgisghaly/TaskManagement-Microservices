619e89ef2bf8d49b2f54d199cf58a1c7d23adfd4
Antony Girgis <antony26429122@gmail.com>
2026-08-25 17:23:46 +0300

    feat: Add DbContext and Migration

---
+++ Tasks.Infrastructure/Configrations/TaskItemConfigration.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Tasks.Domain.Entities;
+
+namespace Tasks.Infrastructure.Configrations
+{
+    internal class TaskItemConfigration : IEntityTypeConfiguration<TaskItem>
+    {
+        public void Configure(EntityTypeBuilder<TaskItem> builder)
+        {
+            builder.HasKey(t => t.Id);
+            builder.Property(t => t.Title).IsRequired().HasMaxLength(200);
+            builder.Property(t => t.Description).HasMaxLength(1000);
+            builder.Property(t => t.Status).HasConversion<string>().HasMaxLength(50);
+            builder.Property(t => t.AssignedToUserId).IsRequired();
+        }
+    }
+}
---
+++ Tasks.Infrastructure/DbContexts/TasksDbContext .cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Tasks.Domain.Entities;
+
+namespace Tasks.Infrastructure.DbContexts
+{
+    public class TasksDbContext : DbContext
+    {
+        public TasksDbContext(DbContextOptions<TasksDbContext> options) : base(options)
+        {
+            
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(TasksDbContext).Assembly);
+        }
+        public DbSet<TaskItem> Tasks => Set<TaskItem>();
+    }
+}
