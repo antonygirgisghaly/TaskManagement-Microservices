b31ae33621a3e791a85fc8a3d114e3b506d5f22d
Antony Girgis <antony26429122@gmail.com>
2026-08-29 13:55:09 +0300

    feat: Add Notfication Entity

---
+++ Notifications.Domain/Entities/Notfication.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Notifications.Domain.Entities
+{
+    public class Notfication
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public string Message { get; set; } = default!;
+        public bool IsRead { get; set; } = false;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
