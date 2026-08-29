29d7173bacd7f783859b1a5c640d8e0ed62e6658
Antony Girgis <antony26429122@gmail.com>
2026-08-29 11:31:09 +0300

    feat : Add TasksController

---
+++ Tasks.Api/Controllers/TasksController.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.IdentityModel.Tokens.Jwt;
+using Tasks.Application.Dtos;
+using Tasks.Application.Interfaces;
+
+namespace Tasks.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class TasksController : ControllerBase
+    {
+        private readonly ITaskService _taskService;
+
+        public TasksController(ITaskService taskService)
+        {
+            _taskService = taskService;
+        }
+
+        private Guid GetUserId()
+        {
+            var userIdClaim = User.FindFirst(JwtRegisteredClaimNames.Sub)?.Value;
+            return Guid.Parse(userIdClaim!);
+        }
+        [HttpPost]
+        public async Task<ActionResult<TaskResponseDto>> Create([FromBody] CreateTaskRequestDto request,CancellationToken ct = default)
+        {
+            var userId = GetUserId();
+            var result = await _taskService.CreateAsync(request, userId, ct);
+            if (!result.IsSuccess)
+                return BadRequest(result.Error);
+
+            return Ok(result.Data);
+        }
+        [HttpGet]
+        public async Task<ActionResult<List<TaskResponseDto>>> GetMyTasks(CancellationToken ct = default)
+        {
+            var userId = GetUserId();
+            var result = await _taskService.GetMyTasksAsync(userId, ct);
+            if (!result.IsSuccess)
+                return NotFound("No tasks found.");
+            return Ok(result.Data);
+        }
+        [HttpGet("{taskId}")]
+        public async Task<ActionResult<TaskResponseDto>> GetTaskById(Guid taskId, CancellationToken ct = default)
+        {
+            var userId = GetUserId();
+            var result = await _taskService.GetTaskByIdAsync(taskId, userId, ct);
+            if (!result.IsSuccess)
+                return NotFound(result.Error);
+            return Ok(result.Data);
+        }
+        [HttpPut("{id}")]
+        public async Task<ActionResult<TaskResponseDto>> Update(Guid id, [FromBody] UpdateTaskRequestDto request, CancellationToken ct)
+        {
+            var result = await _taskService.UpdateAsync(id, request, GetUserId(), ct);
+
+            if (!result.IsSuccess)
+                return BadRequest(new { error = result.Error });
+
+            return Ok(result.Data);
+        }
+        [HttpPatch("{id}/status")]
+        public async Task<ActionResult<TaskResponseDto>> UpdateStatus(Guid id, [FromBody] UpdateTaskStatusRequestDto request, CancellationToken ct)
+        {
+            var result = await _taskService.UpdateStatusAsync(id, request, GetUserId(), ct);
+
+            if (!result.IsSuccess)
+                return BadRequest(new { error = result.Error });
+
+            return Ok(result.Data);
+        }
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(Guid id, CancellationToken ct)
+        {
+            var result = await _taskService.DeleteAsync(id, GetUserId(), ct);
+
+            if (!result.IsSuccess)
+                return NotFound(new { error = result.Error });
+
+            return NoContent();
+        }
+    }
+}
--- Tasks.Api/Program.cs
+++ Tasks.Api/Program.cs
@@ -1,9 +1,14 @@
 
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System.Text;
 using Tasks.Application.Interfaces;
-using Tasks.Infrastructure.Reposatories;
+using Tasks.Application.Services;
+using Tasks.Application.Settings;
 using Tasks.Infrastructure.DbContexts;
-
+using Tasks.Infrastructure.Reposatories;
 namespace Tasks.Api
 {
     public class Program
@@ -14,7 +19,11 @@ namespace Tasks.Api
 
             // Add services to the container.
 
-            builder.Services.AddControllers();
+            builder.Services.AddControllers()
+                        .AddJsonOptions(options =>
+                        {
+                            options.JsonSerializerOptions.Converters.Add(new System.Text.Json.Serialization.JsonStringEnumConverter());
+                        });
             // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
             builder.Services.AddOpenApi();
 
@@ -22,8 +31,37 @@ namespace Tasks.Api
                  options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
             builder.Services.AddScoped<ITaskReposatory, TaskReposatoriey>();
+            builder.Services.AddScoped<ITaskService, TaskService>();
+            var jwtSettings = builder.Configuration.GetSection("JwtSettings").Get<JwtSettings>();
+
+            builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.MapInboundClaims = false;
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = jwtSettings!.Issuer,
+                    ValidAudience = jwtSettings.Audience,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings.SecretKey))
+                };
+            });
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
             var app = builder.Build();
 
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
@@ -32,6 +70,7 @@ namespace Tasks.Api
 
             app.UseHttpsRedirection();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
 
