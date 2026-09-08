8a16c10c106cf9b95e597024acd6dc27fd17e83c
Antony Girgis <antony26429122@gmail.com>
2026-09-08 14:09:55 +0300

    feat : Add Notfications Controllers

---
+++ Notifications.Api/Controllers/NotificationsController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Notifications.Api.Authorization;
+using Notifications.Application.Dtos;
+using Notifications.Application.Interfaces;
+using System.IdentityModel.Tokens.Jwt;
+
+namespace Notifications.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NotificationsController : ControllerBase
+    {
+        private readonly INotificationService _notificationService;
+
+        public NotificationsController(INotificationService notificationService)
+        {
+            _notificationService = notificationService;
+        }
+
+        private Guid GetUserId()
+        {
+            var userIdClaim = User.FindFirst(JwtRegisteredClaimNames.Sub)?.Value;
+            return Guid.Parse(userIdClaim!);
+        }
+        //So why we don't need to use [Authorize] on Create endpoint bec. the create endpoint
+        //is used by task.api the system itself not user service-to-service Authontication so we implment Api key Authontication to use it to make
+        //task.api able to call this endpoint without user authentication.
+        [RequireApiKey]
+        [HttpPost]
+        public async Task<ActionResult<NotificationResponseDto>> Create([FromBody] CreateNotificationRequestDto request, CancellationToken ct)
+        {
+            var result = await _notificationService.CreateAsync(request, ct);
+
+            if (!result.IsSuccess)
+                return BadRequest(new { error = result.Error });
+
+            return Ok(result.Data);
+        }
+
+        [Authorize]
+        [HttpGet]
+        public async Task<ActionResult<List<NotificationResponseDto>>> GetMyNotifications(CancellationToken ct)
+        {
+            var result = await _notificationService.GetMyNotificationsAsync(GetUserId(), ct);
+            return Ok(result.Data);
+        }
+
+        [Authorize]
+        [HttpPatch("{id}/read")]
+        public async Task<ActionResult> MarkAsRead(Guid id, CancellationToken ct)
+        {
+            var result = await _notificationService.MarkAsReadAsync(id, GetUserId(), ct);
+
+            if (!result.IsSuccess)
+                return NotFound(new { error = result.Error });
+
+            return NoContent();
+        }
+
+        [Authorize]
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(Guid id, CancellationToken ct)
+        {
+            var result = await _notificationService.DeleteAsync(id, GetUserId(), ct);
+
+            if (!result.IsSuccess)
+                return NotFound(new { error = result.Error });
+
+            return NoContent();
+        }
+    }
+}
--- Notifications.Api/Program.cs
+++ Notifications.Api/Program.cs
@@ -1,3 +1,13 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Notifications.Application.Interfaces;
+using Notifications.Application.Services;
+using Notifications.Infrastructure;
+using Notifications.Infrastructure.Reposatories;
+using Notifications.Application.Settings;
 
 namespace Notifications.Api
 {
@@ -10,19 +20,51 @@ namespace Notifications.Api
             // Add services to the container.
 
             builder.Services.AddControllers();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
             // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
             builder.Services.AddOpenApi();
 
-            var app = builder.Build();
+            builder.Services.AddDbContext<NotificationsDbContext>(options =>
+                   options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+  
+            builder.Services.AddScoped<INotificationRepository, NotificationRepository>();
+            builder.Services.AddScoped<INotificationService, NotficationService>();
 
-            // Configure the HTTP request pipeline.
+            // JWT Authentication
+            var jwtSettings = builder.Configuration.GetSection("JwtSettings").Get<JwtSettings>();
+
+            builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.MapInboundClaims = false;
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = jwtSettings!.Issuer,
+                    ValidAudience = jwtSettings.Audience,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings.SecretKey))
+                };
+            });
+
+            var app = builder.Build();
             if (app.Environment.IsDevelopment())
             {
+                app.UseSwagger();
+                app.UseSwaggerUI();
                 app.MapOpenApi();
             }
 
             app.UseHttpsRedirection();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
 
